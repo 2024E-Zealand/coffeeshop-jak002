a04f4f3754ef5c192311b818a210d3aaa422768e
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-09-02 14:32:24 +0200

    task 0 done(???)

---
+++ CoffeeShopConsoleAppNet60/Coffee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShopConsoleAppNet60
+{
+    public class Coffee
+    {
+
+        public int Price()
+        {
+            return 20;
+        }
+    }
+}
