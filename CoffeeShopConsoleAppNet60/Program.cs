5251f727801fcc6ef67931458200f750435f576d
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-09-02 14:46:49 +0200

    task 6 done

--- CoffeeShopConsoleAppNet60/Program.cs
+++ CoffeeShopConsoleAppNet60/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CoffeeShopConsoleAppNet60;
+
 Console.WriteLine("Hello, World!");
+
+Coffee c1 = new BlackCoffee();
+Coffee c2 = new Cortado();
+Coffee c3 = new Latte();
+
+List<Coffee> CoffeeList  = new List<Coffee>();
+CoffeeList.Add(c1);
+CoffeeList.Add(c2);
+CoffeeList.Add(c3);
+
+foreach (Coffee coffee in CoffeeList)
+{
+    Console.WriteLine($"{coffee.Strength()} coffee for {coffee.Price()}kr");
+}
